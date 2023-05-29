623d19201cd97ca374f802468df0fee7d6c9ec62
fabioscielzo <fabioscielzo98@gmail.com>
2023-05-29 18:33:10 +0200

    n

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import itertools
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 import uuid
 import base64 
 
